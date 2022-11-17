5227f1a93eae2c2d54c6ff54e1641e5c26f4a96c
RJG091 <rjgaborno100@student.hbuhsd.edu>
2022-11-17 10:03:23 -0800

    UnityStuff2

---
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(-2, 25, 10);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    { 
+        //math stuff to follow player 
+        transform.position = player.transform.position + offset;
+    }
+}
